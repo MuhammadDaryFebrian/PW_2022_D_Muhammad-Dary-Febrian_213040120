c687f055f0b11c9aa0cc4c1686c8a8eabec02461
MuhammadDaryFebrian <96764529+MuhammadDaryFebrian@users.noreply.github.com>
2022-03-24 16:22:15 +0700

    Add files via upload

---
+++ kuliah/pertemuan6/kuliah_latihan1.php
@@ -0,0 +1,37 @@
+<?php
+// Array Associative
+// Array yang key nya ber-asosiasi
+
+$mahasiswa = [
+[  
+      "nama" => "Sandhika Galih",
+    "npm" => "043040023",
+    "email" => "sandhikagalih@mail.unpas.ac.id",
+    "jurusan" => "Teknik Informatika",
+    "gambar" => "sandhika.jpeg"
+],    
+[
+    "nama" => "Doddy Ferdiansyah",
+    "npm" => "103040001",
+    "email" => "doddy@gmail.com",
+    "jurusan" => "Teknik Mesin",
+    "gambar" => "doddy.jpeg"
+],
+[
+    "nama" => "Erik",
+    "npm" => "133040321",
+    "email" => "erik@mail.unpas.ac.id",
+    "jurusan" => "Teknik Informatika"
+    "gambar" => "Erik.jpeg"
+],
+[
+    "nama" => "Anggoro Ari",
+    "npm" => "1033040123",
+    "email" => "anggoro@gmail.com",
+    "jurusan" => "Teknik Industri",
+    "gambar" => "anggoro.jpeg"
+],
+];
+
+//var_dump ($mahasiswa);
+?>
